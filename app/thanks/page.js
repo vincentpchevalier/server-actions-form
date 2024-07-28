6e9013c9af6417aeb7200761132ecd00d0ee1d5d
vincentpchevalier <vincentpchevalier@proton.me>
2024-07-27 20:33:29 -0400

    style thanks page

--- app/thanks/page.js
+++ app/thanks/page.js
@@ -1,15 +1,21 @@
 'use client';
 
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation'; // can only use on client side
+
+import styles from '@/app/page.module.css';
 
 function Page() {
-	let tally = localStorage.getItem('tally');
+	// let tally = localStorage.getItem('tally');
+	const name = useSearchParams().get('name');
 
 	return (
 		<div>
 			<h2>
-				Thank you xxxxx for your complaint. You have submitted {tally}{' '}
-				complaints.
+				Thank you <span className={styles.name}>{name}</span> for your
+				complaint.
+				{/* You have submitted {tally}{' '}
+				complaints. */}
 			</h2>
 
 			<Link href="/">Return to Form</Link>
